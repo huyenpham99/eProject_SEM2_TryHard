42679fda29a5ea9c63195bfd6b2393034699bb75
Đạt Nguyễn <53225576+tiendat21896@users.noreply.github.com>
2020-06-26 22:15:28 +0700

    bldade

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $table = "categories";
+
+    public $fillable = [
+        "category_name",
+    ];
+
+    public function Products(){
+        return $this->hasMany("App\Product"); // tra ve 1 collection
+    }
+
+    public function getRouteKeyName()
+    {
+        return "slug";
+    }
+
+    public function getCategoryUrl(){
+        return url("/category/{$this->__get("slug")}");
+    }
 }
