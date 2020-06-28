58f4b39df02bf795cd4e7f19e3ad6f45e800b540
Nguyen Thanh Tuan <tuantuna955@gmail.com>
2020-06-28 10:28:35 +0700

    View + Set Access + Create User

--- routes/admin.php
+++ routes/admin.php
@@ -25,7 +25,7 @@ Route::get("/list-user", "UserController@listUser")->middleware('admin1');
 Route::get("/new-manager", "UserController@newManager")->middleware('admin1');
 Route::post("/save-manager", "UserController@saveManager")->middleware('admin1');
 
-Route::put("/update-access/{id}","UserController@updateAccess");
-Route::get("/edit-user/{id}", "UserController@editUser");
-Route::put("/update-user/{id}", "UserController@updateUser");
-Route::get("/view-user/{id}","UserController@viewUser");
+Route::put("/update-access/{id}","UserController@updateAccess")->middleware('admin1');
+Route::get("/edit-user/{id}", "UserController@editUser")->middleware('admin1');
+Route::put("/update-user/{id}", "UserController@updateUser")->middleware('admin1');
+Route::get("/view-user/{id}","UserController@viewUser")->middleware('admin1');
