6b818cf577bf3b14a97ed3a48692ee2cd91295a5
Đạt Nguyễn <53225576+tiendat21896@users.noreply.github.com>
2020-06-28 12:49:24 +0700

    Merge branch 'master' of https://github.com/huyenpham99/eProject_SEM2_TryHard into DAT
    # Conflicts:
    #	app/Http/Controllers/HomeController.php

--- resources/views/components/frontend/header.blade.php
+++ resources/views/components/frontend/header.blade.php
@@ -54,6 +54,7 @@
                     </a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
 {{--                        //--}}
+{{--                        //--}}
                         @csrf
                     </form>
                 @endguest
