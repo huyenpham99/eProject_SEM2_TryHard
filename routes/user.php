bedc8d9d2d010fe67799380b52f803ce219a7866
Nguyễn Minh Thắng <49161576+Thang-1996@users.noreply.github.com>
2020-06-28 14:40:21 +0700

    a

--- routes/user.php
+++ routes/user.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 ////
 //Route::get('/', 'HomeController@index');
 //
@@ -10,7 +10,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 //Route::get("/product/{product:slug}", "HomeController@product");
 //Route::get("/blog/{blog:slug}", "HomeController@product");
-Route::get('/', 'HomeController@index');
+//Route::get('/', 'HomeController@index');
 
 Route::get("/category/{category:slug}", "HomeController@category")->name("category");
 
