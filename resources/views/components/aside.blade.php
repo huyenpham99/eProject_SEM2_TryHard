144f7845ef930eb5820d8634e1386008efe63d3a
Đạt Nguyễn <53225576+tiendat21896@users.noreply.github.com>
2020-06-28 14:52:12 +0700

    Aside(DAT)

--- resources/views/components/aside.blade.php
+++ resources/views/components/aside.blade.php
@@ -43,35 +43,29 @@
                 </li>
                 <li class="app-sidebar__heading">WEB</li>
                 @if(Auth::user()->role === 1)
+                    <li>
+                        <a href="#">
+                            <i class="metismenu-icon pe-7s-diamond"></i>
+                            Admin
+                            <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+                        </a>
+                        <ul>
+                            <li>
+                                <a href="{{url("/admin/new-manager")}}">
+                                    <i class="metismenu-icon"></i>
+                                    New Manager
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{url("/admin/list-user")}}">
+                                    <i class="metismenu-icon"></i>
+                                    List Manager
+                                </a>
+                            </li>
+                        </ul>
+                @else
+                @endif
                 <li>
-                    <a href="#">
-                        <i class="metismenu-icon pe-7s-diamond"></i>
-                        Category
-                        <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
-                    </a>
-
-                    <ul>
-                        <li>
-
-                            <a href="{{url("/admin/new-manager")}}">
-
-                                <i class="metismenu-icon"></i>
-                                New Manager
-                            </a>
-                        </li>
-                        <li>
-
-                            <a href="{{url("/admin/list-user")}}">
-
-
-                                <i class="metismenu-icon"></i>
-                                List Manager
-                            </a>
-                        </li>
-                    </ul>
-                    @else
-                        @endif
-
                     <a href="#">
                         <i class="metismenu-icon pe-7s-diamond"></i>
                         Product
@@ -103,7 +97,6 @@
                     </a>
                     <ul>
                         <li>
-
                             <a href="{{url("/admin/new-category")}}">
 
                                 <i class="metismenu-icon"></i>
@@ -111,7 +104,6 @@
                             </a>
                         </li>
                         <li>
-
                             <a href="{{url("/admin/list-category")}}">
 
                                 <i class="metismenu-icon"></i>
