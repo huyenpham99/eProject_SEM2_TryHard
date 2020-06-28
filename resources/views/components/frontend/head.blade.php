4cf9043be9aa9efe7cd5f1737a7fb1527d4cf1b8
Đạt Nguyễn <53225576+tiendat21896@users.noreply.github.com>
2020-06-28 11:55:56 +0700

    frontend

---
+++ resources/views/components/frontend/head.blade.php
@@ -0,0 +1,33 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- favicon -->
+<link rel="shortcut icon" type="image/png" href="{{asset("frontend/favicon.png")}}"/>
+<link rel="shortcut icon" type="image/png" href="{{asset("frontend/favicon.png")}}"/>
+<title>Madang</title>
+<!-- style lists -->
+<link href="{{asset("frontend/css/bootstrap.min.css")}}" rel="stylesheet">
+<link href="{{asset("frontend/css/animate.css")}}" rel="stylesheet">
+<link href="{{asset("frontend/css/hover.css")}}" rel="stylesheet">
+<link href="{{asset("frontend/css/font-awesome.css")}}" rel="stylesheet">
+<link href="{{asset("frontend/css/jquery.bxslider.css")}}" rel="stylesheet">
+<link href="{{asset("frontend/css/owl.carousel.css")}}" rel="stylesheet">
+<link href="{{asset("frontend/css/owl.transitions.css")}}" rel="stylesheet">
+<link rel="stylesheet" href="{{asset("frontend/css/feature-carousel.css")}}">
+<link href="{{asset("frontend/css/jquery-ui-1.9.2.custom.css")}}" rel="stylesheet">
+<link href="{{asset("frontend/css/style.css")}}" rel="stylesheet">
+
+<!-- montserrat font embed -->
+<link href='https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,700' rel='stylesheet' type='text/css'>
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<style>
+    /******* loader image before page load ******/
+    .no-js #loader {display: none;}.js #loader { display: block; position: absolute; left: 100px; top: 0;}.se-pre-con {position: fixed;left: 0px;top: 0px;width: 100%;height: 100%;z-index: 9999;background: url(images/Preloader_4.gif) center no-repeat #fff;}
+</style>
