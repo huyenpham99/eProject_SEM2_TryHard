d7e564d97be9249ff77943112baa425087459933
Đạt Nguyễn <53225576+tiendat21896@users.noreply.github.com>
2020-06-29 11:18:15 +0700

    Frontend(DAT)

--- resources/views/components/frontend/head.blade.php
+++ resources/views/components/frontend/head.blade.php
@@ -1,34 +1,20 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- favicon -->
-<link rel="shortcut icon" type="image/png" href="{{asset("frontend/favicon.png")}}"/>
-<link rel="shortcut icon" type="image/png" href="{{asset("frontend/favicon.png")}}"/>
-<title>Madang</title>
-<!-- style lists -->
-<link href="{{asset("frontend/css/bootstrap.min.css")}}" rel="stylesheet">
-<link href="{{asset("frontend/css/animate.css")}}" rel="stylesheet">
-<link href="{{asset("frontend/css/hover.css")}}" rel="stylesheet">
-<link href="{{asset("frontend/css/font-awesome.css")}}" rel="stylesheet">
-<link href="{{asset("frontend/css/jquery.bxslider.css")}}" rel="stylesheet">
-<link href="{{asset("frontend/css/owl.carousel.css")}}" rel="stylesheet">
-<link href="{{asset("frontend/css/owl.transitions.css")}}" rel="stylesheet">
-<link rel="stylesheet" href="{{asset("frontend/css/feature-carousel.css")}}">
-<link href="{{asset("frontend/css/jquery-ui-1.9.2.custom.css")}}" rel="stylesheet">
-<link href="{{asset("frontend/css/style.css")}}" rel="stylesheet">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+<link rel="shortcut icon" href="frontend/images/favicon.ico"/>
+<title>Organik Shop &#8211; Homepage Main | Organik HTML Template</title>
 
-
-<!-- montserrat font embed -->
-<link href='https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,700' rel='stylesheet' type='text/css'>
-
+<link rel="stylesheet" href="frontend/css/bootstrap.min.css" type="text/css" media="all"/>
+<link rel="stylesheet" href="frontend/css/font-awesome.min.css" type="text/css" media="all" />
+<link rel="stylesheet" href="frontend/css/ionicons.min.css" type="text/css" media="all" />
+<link rel="stylesheet" href="frontend/css/owl.carousel.css" type="text/css" media="all"/>
+<link rel="stylesheet" href="frontend/css/owl.theme.css" type="text/css" media="all"/>
+<link rel="stylesheet" href="frontend/css/settings.css" type="text/css" media="all"/>
+<link rel="stylesheet" href="frontend/css/style.css" type="text/css" media="all"/>
+<link rel="stylesheet" href="frontend/css/custom.css" type="text/css" media="all"/>
+<link href="http://fonts.googleapis.com/css?family=Great+Vibes%7CLato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet"/>
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
-
-<style>
-    /******* loader image before page load ******/
-    .no-js #loader {display: none;}.js #loader { display: block; position: absolute; left: 100px; top: 0;}.se-pre-con {position: fixed;left: 0px;top: 0px;width: 100%;height: 100%;z-index: 9999;background: url(images/Preloader_4.gif) center no-repeat #fff;}
-</style>
--- resources/views/components/frontend/header.blade.php
+++ resources/views/components/frontend/header.blade.php
@@ -1,71 +1,241 @@
-<!-- ============== Header starts ============== -->
-<header>
+<div class="topbar">
     <div class="container">
         <div class="row">
-            <!-- ============== Left logo block starts ============== -->
-            <div class="col-xs-12 col-sm-3 logo-block">
-                <figure><a href="home-version1.html"><img class="img-responsive" src="images/madang-logo.png" alt="Madang Logo" /></a></figure>
+            <div class="col-md-6">
+                <div class="topbar-text">
+                    <span>Work time: Monday - Friday: 08AM-06PM</span>
+                    <span>Saturday - Sunday: 10AM-06PM</span>
+                </div>
             </div>
-            <!-- ============== Left logo block ends ============== -->
-
-            <!-- ============== Main navigation starts ============== -->
-            <div class="col-xs-12 col-sm-7 menu-block">
-                <nav class="navbar navbar-default">
-                    <div class="container-fluid">
-                        <!-- Brand and toggle get grouped for better mobile display -->
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                        </div>
-                        <!-- Collect the nav links, forms, and other content for toggling -->
-                        <div class="collapse">
-                            <ul class="nav navbar-nav text-right pull-right">
-                                <li><a href="{{asset("/")}}">Home</a></li>
-                                <li class="active"><a href="{{asset("/about")}}">About Us</a></li>
-                                <li><a href="{{asset("/shop")}}">Shop</a></li>
-                                <li><a href="{{asset("/programs")}}">Programs</a></li>
-                                <li><a href="{{asset("/blog")}}">Blog</a></li>
+            <div class="col-md-6">
+                <div class="topbar-menu">
+                    <ul class="topbar-menu">
+                        <li class="dropdown">
+                            <a href="#">Languages</a>
+                            <ul class="sub-menu">
+                                <li><a href="#">English</a></li>
+                                <li><a href="#">Français</a></li>
                             </ul>
-                        </div><!-- /.navbar-collapse -->
-                    </div><!-- /.container-fluid -->
-                </nav>
-            </div>
-            <div class="logout" style="position: relative;margin-top: 15px">
-                @guest
-                    <li class="float-right" style="list-style: none;"><a href="{{url("/login")}}"
-                                                                         style="border-radius: 20px; width: 100px; height: 40px"
-                                                                         style="border-radius: 20px; width: 100px; height: 40px;margin-left: 10px"
-                                                                         type="button"
-                                                                         class="btn btn-secondary">Login</a>
-                    </li>
-                @else
-                    <a class="dropdown-item" style="margin-left: 20px!important;position: absolute;top: -5px"
-                       href="{{ route('logout') }}"
-                       onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();">
-                        <li class="float-right" style="list-style: none;">
-                            <button style="border-radius: 20px; width: 100px; height: 40px" type="button"
-                                    class="btn btn-secondary">Logout
-                            </button>
                         </li>
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{--                        //--}}
-                        {{--                        //--}}
-                        {{--                        //--}}
-                        {{--                        //--}}
-                        {{--                        //--}}
-                        {{--                        //--}}
-                        @csrf
-                    </form>
-                @endguest
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">Register</a></li>
+                    </ul>
+                </div>
             </div>
-            <!-- ============== Main navigation ends ============== -->
+        </div>
+    </div>
+</div>
+<header id="header" class="header header-desktop header-2">
+    <div class="top-search">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <form>
+                        <input type="search" class="top-search-input" name="s" placeholder="What are you looking for?" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <a href="index.html" id="logo">
+                    <img class="logo-image" src="frontend/images/logo.png" alt="Organik Logo" />
+                </a>
+            </div>
+            <div class="col-md-9">
+                <div class="header-right">
+                    <nav class="menu">
+                        <ul class="main-menu">
+                            <li class="active dropdown">
+                                <a href="#">Home</a>
+                                <ul class="sub-menu">
+                                    <li><a href="index.html">Organik Main</a></li>
+                                    <li><a href="index-fresh.html">Organik Fresh</a></li>
+                                    <li><a href="index-shop.html">Organik Shop</a></li>
+                                    <li><a href="index-store.html">Organik Store</a></li>
+                                    <li><a href="index-farm.html">Organik Farm</a></li>
+                                    <li><a href="index-house.html">Organik House</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#">Pages</a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item-has-children">
+                                        <a href="#">About Us</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="about-us.html">About us 01</a></li>
+                                            <li><a href="about-us-2.html">About us 02</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="gallery-freestyle.html">Gallery Freestyle</a></li>
+                                    <li><a href="gallery-grid.html">Gallery Grid</a></li>
+                                    <li><a href="404.html">404</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="shortcodes.html">Shortcodes</a>
+                            </li>
+                            <li class="dropdown mega-menu">
+                                <a href="{{url("/shop")}}">Shop</a>
+                                <ul class="sub-menu">
+                                    <li>
+                                        <div class="mega-menu-content">
+                                            <div class="row">
+                                                <div class="col-sm-3">
+                                                    <div class="pt-4 pb-4">
+                                                        <h3>Shop Pages</h3>
+                                                        <ul>
+                                                            <li><a href="my-account.html">My Account</a></li>
+                                                            <li><a href="cart-empty.html">Empty Cart</a></li>
+                                                            <li><a href="cart.html">Cart</a></li>
+                                                            <li><a href="checkout.html">Check Out</a></li>
+                                                            <li><a href="wishlist.html">Wishlist</a></li>
+                                                            <li><a href="shop.html">Shop</a></li>
+                                                            <li><a href="shop-list.html">Shop List</a></li>
+                                                            <li><a href="shop-detail.html">Single Product</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-2">
+                                                    <div class="pt-4 pb-4">
+                                                        <h3>Fruits</h3>
+                                                        <ul>
+                                                            <li><a href="#">Seville Orange</a></li>
+                                                            <li><a href="#">Aurore Grape</a></li>
+                                                            <li><a href="#">Tieton Cherry</a></li>
+                                                            <li class="new"><a href="#">Tomato Juice</a></li>
+                                                            <li><a href="#">Cauliflower</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-2">
+                                                    <div class="pt-4 pb-4">
+                                                        <h3>Featured</h3>
+                                                        <ul>
+                                                            <li><a href="#">Sprouting Broccoli</a></li>
+                                                            <li class="sale"><a href="#">Chinese Cabbage</a></li>
+                                                            <li><a href="#">Cara Orange</a></li>
+                                                            <li><a href="#">Cauliflower</a></li>
+                                                            <li><a href="#">Tomato Juice</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-2">
+                                                    <div class="pt-4 pb-4">
+                                                        <h3>Best Seller</h3>
+                                                        <ul>
+                                                            <li><a href="#">Uvina Grape</a></li>
+                                                            <li><a href="#">Seville Orange</a></li>
+                                                            <li><a href="#">Aurore Grape</a></li>
+                                                            <li><a href="#">Tieton Cherry</a></li>
+                                                            <li class="new"><a href="#">Tomato Juice</a></li>
+                                                            <li><a href="#">Sprouting Broccoli</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-3">
+                                                    <div class="pt-4 pb-4">
+                                                        <img src="frontend/images/megamenu_ads.jpg" alt="" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="">Blog</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog.html">Blog List</a></li>
+                                    <li><a href="blog-classic.html">Blog Classic</a></li>
+                                    <li><a href="blog-masonry.html">Blog Masonry</a></li>
+                                    <li><a href="blog-detail.html">Blog Single</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="contact-us.html">Contact</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div class="btn-wrap">
+                        <div class="mini-cart-wrap">
+                            <div class="mini-cart">
+                                <div class="mini-cart-icon" data-count="2">
+                                    <i class="ion-bag"></i>
+                                </div>
+                            </div>
+                            <div class="widget-shopping-cart-content">
+                                <ul class="cart-list">
+                                    <li>
+                                        <a href="#" class="remove">×</a>
+                                        <a href="shop-detail.html">
+                                            <img src="frontend/images/shop/thumb/shop_1.jpg" alt="" />
+                                            Orange Juice&nbsp;
+                                        </a>
+                                        <span class="quantity">1 × $12.00</span>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="remove">×</a>
+                                        <a href="shop-detail.html">
+                                            <img src="frontend/images/shop/thumb/shop_2.jpg" alt="" />
+                                            Aurore Grape&nbsp;
+                                        </a>
+                                        <span class="quantity">1 × $9.00</span>
+                                    </li>
+                                </ul>
+                                <p class="total">
+                                    <strong>Subtotal:</strong>
+                                    <span class="amount">$21.00</span>
+                                </p>
+                                <p class="buttons">
+                                    <a href="cart.html" class="view-cart">View cart</a>
+                                    <a href="checkout.html" class="checkout">Checkout</a>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="top-search-btn-wrap">
+                            <div class="top-search-btn">
+                                <a href="javascript:void(0);">
+                                    <i class="ion-search"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </header>
-<!-- ============== Header ends ============== -->
+<header class="header header-mobile">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-2">
+                <div class="header-left">
+                    <div id="open-left"><i class="ion-navicon"></i></div>
+                </div>
+            </div>
+            <div class="col-xs-8">
+                <div class="header-center">
+                    <a href="index.html" id="logo-2">
+                        <img class="logo-image" src="images/logo.png" alt="Organik Logo" />
+                    </a>
+                </div>
+            </div>
+            <div class="col-xs-2">
+                <div class="header-right">
+                    <div class="mini-cart-wrap">
+                        <a href="cart.html">
+                            <div class="mini-cart">
+                                <div class="mini-cart-icon" data-count="2">
+                                    <i class="ion-bag"></i>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
