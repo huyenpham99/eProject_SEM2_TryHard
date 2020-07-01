cb8f6de161656f2c2d33ef3d0ebf1aa4ada71247
Nguyen Thanh Tuan <tuantuna955@gmail.com>
2020-07-01 21:49:40 +0700

    Giao diện change + login + register

--- resources/views/auth/changepassword.blade.php
+++ resources/views/auth/changepassword.blade.php
@@ -1,43 +1,78 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<form id="form-change-password" role="form" method="POST" action="{{ url('change-password') }}" novalidate class="form-horizontal">
-    <div class="user">
-        <p>Current User : {{\Illuminate\Support\Facades\Auth::user()->__get("name")}}</p>
-    </div>
-    <div class="col-md-9">
-        <label for="current-password" class="col-sm-4 control-label">Current Password</label>
-        <div class="col-sm-8">
-            <div class="form-group">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <input type="password" class="form-control" id="current-password" name="current-password" placeholder="Password">
+
+@extends('layouts.app')
+
+@section('content')
+    <body class="bg-gradient-primary register" style="background-color: #4e73df;
+    background-image: linear-gradient(180deg,#fee06e 10%,#bbdd7f 100%);
+    background-size: cover;">
+    <div class="container">
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0" style="display: flex; flex-direction: row">
+                        <!-- Nested Row within Card Body -->
+                        <div class="col-lg-6 d-none d-lg-block bg-login-image" style="background: url({{asset("frontend/images/oranges.png")}});
+                            background-position: center;
+                            background-size: cover; width: 450px; height: 560px"></div>
+                        <div class="col-lg-6">
+                            <div class="card-header">{{ __('Change password') }}</div>
+                            <div class="card-body">
+                                <form id="form-change-password" role="form" method="POST" action="{{ url('change-password') }}" novalidate class="form-horizontal">
+                                    <div class="user col-12">
+                                        <p>Current User : {{\Illuminate\Support\Facades\Auth::user()->__get("name")}}</p>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <label for="current-password" class="col-sm-12 control-label">Current Password</label>
+                                        <div class="col-sm-12">
+                                            <div class="form-group">
+                                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                                <input type="password" class="form-control" id="current-password" name="current-password" placeholder="Password">
+                                            </div>
+                                        </div>
+                                        <label for="password" class="col-sm-12 control-label">New Password</label>
+                                        <div class="col-sm-12">
+                                            <div class="form-group">
+                                                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                                            </div>
+                                        </div>
+                                        <label for="password_confirmation" class="col-sm-12 control-label">Re-enter Password</label>
+                                        <div class="col-sm-12">
+                                            <div class="form-group">
+                                                <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Re-enter Password">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-sm-offset-5 col-sm-6">
+                                            <button type="submit" class="btn btn-danger">Change</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <label for="password" class="col-sm-4 control-label">New Password</label>
-        <div class="col-sm-8">
-            <div class="form-group">
-                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-            </div>
-        </div>
-        <label for="password_confirmation" class="col-sm-4 control-label">Re-enter Password</label>
-        <div class="col-sm-8">
-            <div class="form-group">
-                <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Re-enter Password">
-            </div>
-        </div>
     </div>
-    <div class="form-group">
-        <div class="col-sm-offset-5 col-sm-6">
-            <button type="submit" class="btn btn-danger">Submit</button>
-        </div>
-    </div>
-</form>
-</body>
-</html>
+    </body>
+{{--    <body class="bg-gradient-primary register" style="background-color: #4e73df;--}}
+{{--    background-image: linear-gradient(180deg,#fee06e 10%,#bbdd7f 100%);--}}
+{{--    background-size: cover;">--}}
+
+{{--        <div class="container">--}}
+{{--            <div class="row justify-content-center">--}}
+{{--                    <div class="card col-md-12">--}}
+{{--                        <div class="col-lg-6 d-none d-lg-block bg-login-image" style="background: url({{asset("frontend/images/oranges.png")}});--}}
+{{--                            background-position: center;--}}
+{{--                            background-size: cover; width: 450px; height: 560px"></div>--}}
+{{--                        --}}
+{{--                    </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    <body/>--}}
+@endsection
+
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,88 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <body class="bg-gradient-primary register" style="    background: linear-gradient(180deg, #e12f45 20%, #e5c146 80%) !important;
+    background-size: cover;">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <div class="container">
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-7 d-none d-lg-block bg-register-image" style="background: url({{asset("/frontend/images/oranges.png")}});
+    background-position: center;
+    background-size: cover;">
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                    </div>
+                    <div class="col-lg-5">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                            </div>
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
+                                <div class="form-group row">
+                                    <div class="col-sm-12 mb-3 mb-sm-0">
+                                        <input id="name" type="text"  placeholder="UserName" class="form-control form-control-user @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                @error('name')
+                                        @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input id="email" type="email" placeholder="Email Address" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                    @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
+                                    @enderror
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input id="password" type="password" placeholder="Password" class="form-control form-control-user @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                        @error('password')
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                        @enderror
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input id="password-confirm" placeholder="Confirm Password" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">                                </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary" style="width: 100%">
                                     {{ __('Register') }}
                                 </button>
+                                <hr>
+                                <a href="{{url("/")}}" class="btn btn-google btn-user btn-block">
+                                    <i class="fab fa-google fa-fw"></i> Register with Google
+                                </a>
+                                <a href="{{url("/")}}" class="btn btn-facebook btn-user btn-block">
+                                    <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
+                                </a>
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link"  style="width: 100%" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                            <div class="text-center">
+                                <a class="small" href="{{route('login')}}">Already have an account? Login!</a>
                             </div>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
-</div>
+    </body>
 @endsection
