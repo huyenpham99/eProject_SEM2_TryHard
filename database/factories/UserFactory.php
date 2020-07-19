903812ec294df10dce3c44a02daa43c169e53830
Nguyễn Minh Thắng <49161576+Thang-1996@users.noreply.github.com>
2020-07-19 15:46:04 +0700

    fix push

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,12 +19,10 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => 'admin',
-        'email' => 'admin@gmail.com',
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => bcrypt('12345678'), // password
-        'account_status' => 'admin',
-        'role' => 1,
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
 });
