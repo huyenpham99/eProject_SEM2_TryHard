2f2eab97ff6956904f6127ef887e32dd1a395333
Nguyen Thanh Tuan <tuantuna955@gmail.com>
2020-07-21 22:50:39 +0700

    dang nhap bang facebook

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -66,7 +66,7 @@
                                         <a href="{{url("/")}}" class="btn btn-google btn-user btn-block">
                                             <i class="fab fa-google fa-fw"></i> Login with Google
                                         </a>
-                                        <a href="{{url("/")}}" class="btn btn-facebook btn-user btn-block">
+                                        <a href="{{ url('/auth/redirect/facebook') }}" class="btn btn-facebook btn-user btn-block">
                                             <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
                                         </a>
                                     </form>
