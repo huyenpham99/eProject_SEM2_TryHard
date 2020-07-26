4f9b575f1ab85aa966ed8aa021890e0010c395c1
huyenpham99 <53186394+huyenpham99@users.noreply.github.com>
2020-07-27 01:18:38 +0700

    sql

--- config/database.php
+++ config/database.php
@@ -15,8 +15,8 @@ return [
     |
     */
 
-//   'default' =>'pgsql',//env('DB_CONNECTION', 'mysql'),
-    'default' =>env('DB_CONNECTION', 'mysql'),
+   'default' =>'pgsql',//env('DB_CONNECTION', 'mysql'),
+//    'default' =>env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
