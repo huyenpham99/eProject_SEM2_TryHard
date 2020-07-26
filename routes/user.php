11400e9e15219e1357e0a85e8ce38a19f54dae11
Nguyễn Minh Thắng <49161576+Thang-1996@users.noreply.github.com>
2020-07-26 16:06:08 +0700

    update

--- routes/user.php
+++ routes/user.php
@@ -14,32 +14,6 @@ Route::get('/', 'HomeController@index');
 
 //Route::get('/', 'HomeController@index');
 
-Route::get("/shop/{category:slug}", "HomeController@shop");
-
-Route::get("/product/{product:slug}", "HomeController@productdetail");
-
-Route::get("/about", "HomeController@about");
-
-Route::get("/blog","HomeController@blog");
-Route::get("/blog/{blog:slug}","HomeController@blogdetail");
-Route::post("/blog/comment","HomeController@saveComment");
-
-Route::get("/shop","HomeController@allShop");
-
-Route::get("/product-detail","HomeController@productdetail");
-
-Route::get("/contact","HomeController@contact");
-
-Route::get("/programcategory","HomeController@programcategory");
-
-Route::get("/program/{id}","HomeController@program");
-
-Route::get("/programdetail/{id}","HomeController@programs_detail");
-
-Route::post("/cart/add/{product}", "HomeController@addToCart");
-
-Route::get("/shopping-cart","HomeController@shoppingCart");
-
 Route::get("/checkout","HomeController@checkout")->middleware("auth");
 
 Route::post("/checkout","HomeController@placeOrder")->middleware("auth");
@@ -50,10 +24,3 @@ Route::post("/cancel-order/{id}","OrderController@cancelOrder")->middleware("aut
 //Route::get("/vn-pay","VNPayController@createPayment")->middleware("auth");
 Route::get("/return-vnpay","VNPayController@return")->middleware("auth");
 Route::get("/return-donate","VNPayController@return1")->middleware("auth");
-Route::get('/donate/{donate:slug}',"DonateController@donateDetail");
-Route::get('/donate',"DonateController@donate");
-Route::post("/update-money/{id}","DonateController@saveMoney");
-
-Route::get("/event", "EventController@listEventFrontEnd");
-Route::get("/event/{event:slug}", "EventController@eventDetails");
-Route::get("/create-ticket-buyer", "EventController@createTicketBuyer");
